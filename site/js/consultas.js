27f92a024239326a0b8a154190aba8bb0c377611
tomasgentilee <tomigentile@gmail.com>
2023-11-11 19:16:36 -0300

    todo

---
+++ site/js/consultas.js
@@ -0,0 +1,19 @@
+const apiUrl = 'http://localhost:8080/api/campaign/active';
+const postData = {
+    description:"mi campaña",
+    name:"tomas",
+    totalAmount:100000,
+    campaignType:"PRIVATE"
+};
+
+fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(postData)
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
